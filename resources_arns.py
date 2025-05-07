19a3e861cda5c2ce66eebe8ce9e915b306f5f52f
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-07 19:10:57 +0530

    Create resources_arns.py

---
+++ resources_arns.py
@@ -0,0 +1,8 @@
+central_resource_arn = [
+    'arn:aws:s3:::mainaccountbvcket',
+    'arn:aws:dynamodb:us-east-1:131332286832:table/testing'
+]
+
+Member_resource_arn = [
+    'arn:aws:s3:::tagtestings3buvket'
+]
