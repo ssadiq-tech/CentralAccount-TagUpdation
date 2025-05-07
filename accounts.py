d66c44f1945786975cc75c59811a3fc6002d65ad
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-07 19:16:41 +0530

    Update and rename accounts.py to config.py

--- accounts.py
+++
@@ -1,7 +0,0 @@
-
-
-# List of target AWS Account IDs
-account_ids = ['148787051713']
-
-# The role name you want to assume in target accounts
-role_name = 'OrganizationAccountAccessRole'
---
+++ config.py
@@ -0,0 +1,17 @@
+
+# The role name you want to assume in target accounts
+role_name = 'OrganizationAccountAccessRole'
+
+# List of target AWS Account IDs
+account_ids = ['148787051713']
+
+
+central_resource_arn = [
+    'arn:aws:s3:::mainaccountbvcket',
+    'arn:aws:dynamodb:us-east-1:131332286832:table/testing'
+]
+
+Member_resource_arn = [
+    'arn:aws:s3:::tagtestings3buvket'
+]
+
