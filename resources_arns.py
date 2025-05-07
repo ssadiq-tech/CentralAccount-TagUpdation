2304e99caf31b27239458fd94fea80a6cbde6494
SHEIK SADIQ <sheiksadiq8099@outlook.com>
2025-05-07 19:20:47 +0530

    Delete resources_arns.py

--- resources_arns.py
+++
@@ -1,8 +0,0 @@
-central_resource_arn = [
-    'arn:aws:s3:::mainaccountbvcket',
-    'arn:aws:dynamodb:us-east-1:131332286832:table/testing'
-]
-
-Member_resource_arn = [
-    'arn:aws:s3:::tagtestings3buvket'
-]
